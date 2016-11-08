0fac225a934d0820dfe22404dad5811bebfd2939
alexhoneygosky <ach53@pitt.edu>
2016-11-08 14:00:27 -0500

    Added test file shells that will have the junit code in them.

---
+++ TestMonkey.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+public class TestMonkey {
+    //@Mock
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateId() {
+        assertEquals(true, true);
+    }          
+}
---
+++ TestMonkeySim.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+public class TestMonkeySim {
+    //@Mock
+
+    @Before
+    public void setUp() throws Exception {
+
+    }      
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
